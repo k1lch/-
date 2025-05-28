acc23a16ea679584aab0075377b4def7bbaf8323
k1lch <168177630+k1lch@users.noreply.github.com>
2025-05-28 13:14:01 +0300

    Add files via upload

---
+++ WinAsynchMethod-master/WinAsynchMethod/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAsynchMethod
+{
+    public partial class Form1 : Form
+    {
+        private delegate int AsyncSumm(int a, int b);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int Summ(int a, int b)
+        {
+            Thread.Sleep(9000);
+            return a + b;
+        }
+
+        private void CallBackMethod(IAsyncResult ar)
+        {
+            AsyncSumm summdelegate = (AsyncSumm)ar.AsyncState;
+            string str = String.Format("Сумма введенных чисел равна {0}", summdelegate.EndInvoke(ar));
+            MessageBox.Show(str, "Результат операции");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                a = Int32.Parse(txbA.Text);
+                b = Int32.Parse(txbB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("При выполнении преобразования типов возникла ошибка");
+                txbA.Text = txbB.Text = "";
+                return;
+            }
+
+            AsyncSumm summdelegate = new AsyncSumm(Summ);
+            AsyncCallback cb = new AsyncCallback(CallBackMethod);
+            summdelegate.BeginInvoke(a, b, cb, summdelegate);
+        }
+
+        private void btnWork_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работа кипит!!!");
+        }
+    }
+}
